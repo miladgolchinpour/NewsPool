a0f7886ca5212764cc765f9eb0b8b09810d4af9a
Milad Golchinpour <miladsoftware@icloud.com>
2021-09-17 09:01:47 +0430

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Milad Golchinpour on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
